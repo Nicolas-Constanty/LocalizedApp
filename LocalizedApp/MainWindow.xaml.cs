49c73476f96fe1ad0e15d3381d6770f9c2fe6f10
Nicolas Constanty <nicolas.constanty@epitech.eu>
2017-01-18 00:19:25 +0100

    Ajoutez des fichiers projet.

---
+++ LocalizedApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LocalizedApp
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Settings _settings_windows = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            App app = (App)Application.Current;
+            app.LoadConfig();
+            //LoadDynamicResource("Languages\\en-EN.xaml");
+        }
+        private void LoadDynamicResource(String StyleToUse)
+        {
+
+            ResourceDictionary dic = new ResourceDictionary { Source = new Uri(StyleToUse, UriKind.Relative) };
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(dic);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (_settings_windows == null)
+                _settings_windows = new Settings();
+            if (_settings_windows.Visibility != Visibility.Visible)
+                _settings_windows.Show();
+            else
+                _settings_windows.Hide();
+        }
+    }
+}
---
+++ LocalizedApp/Settings.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LocalizedApp
+{
+    /// <summary>
+    /// Logique d'interaction pour Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        private String[] _lang_array;
+        private String[] _language_codes;
+
+        public Settings()
+        {
+            Resources.MergedDictionaries.Add(Application.Current.MainWindow.Resources);
+            InitializeComponent();
+            LoadLangCodes();
+            InitComboBoxLabel();
+        }
+
+        private void LoadLangCodes()
+        {
+            String language_codes = App.GetSetting("language_codes");
+            _language_codes = language_codes.Split(',');
+        }
+
+        private void InitComboBoxLabel()
+        {
+            ComboBox_Languages.SelectionChanged -= ComboBox_Languages_SelectionChanged;
+            String languages = App.GetSetting("languages");
+            if (!String.IsNullOrEmpty(languages))
+            {
+                _lang_array = languages.Split(',');
+                for (int i = 0; i < _lang_array.Length; i++)
+                {
+                    ComboBoxItem item = new ComboBoxItem();
+                    item.SetResourceReference(ComboBoxItem.ContentProperty, "_" + _lang_array[i]);
+                    ComboBox_Languages.Items.Add(item);
+                }
+            }
+            ComboBox_Languages.Text = FindResource("_Culture_Lang").ToString();
+            ComboBox_Languages.SelectionChanged += ComboBox_Languages_SelectionChanged;
+        }
+
+        private void ComboBox_Languages_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            App app = App.Current as App;
+            ComboBox cmb = sender as ComboBox;
+            app.LoadLanguage(_language_codes[cmb.SelectedIndex], this);
+        }
+    }
+}
